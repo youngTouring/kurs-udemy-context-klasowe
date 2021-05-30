84aeac8f8bc600a009b4e78365f5f116b1a14978
Mikolaj <mikolajdara@gmail.com>
2021-05-30 16:59:52 +0200

    Changed context

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import { AppContext } from './AppContext'
 
 class Button extends React.PureComponent {
+
+  static contextType = AppContext;
+
   render() {
     return (
-      <AppContext.Consumer>
-        {
-          ({toggleLoggedState}) => (
-            <button 
-              onClick = {toggleLoggedState}>
-              Zmień status użytkownika
-            </button>
-          )
-        }
-      </AppContext.Consumer>
-      
-    );
-  }
+        <button 
+          onClick = {this.context.toggleLoggedState}>
+          Zmień status użytkownika
+        </button>
+      )
+    }
 }
 
 export default Button;
